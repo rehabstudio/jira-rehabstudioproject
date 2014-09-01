21cf8248c4e67c46a9843cc3b55381fa76cb1e3c
Peter McConnell <pemcconnell@googlemail.com>
2014-09-01 14:17:42 +0100

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,25 @@
+jira project plugin
+===================
+
+This plugin is designed to create a tailored Workflow and Issue Scheme project template
+
+Install
+-------
+
+Simply upload the jar file into the plugin section and go.
+
+
+You have successfully created an Atlassian Plugin!
+--------------------------------------------------
+
+Here are the SDK commands you'll use immediately:
+
+* atlas-run   -- installs this plugin into the product and starts it on localhost
+* atlas-debug -- same as atlas-run, but allows a debugger to attach at port 5005
+* atlas-cli   -- after atlas-run or atlas-debug, opens a Maven command line window:
+                 - 'pi' reinstalls the plugin into the running product instance
+* atlas-help  -- prints description for all commands in the SDK
+
+Full documentation is always available at:
+
+https://developer.atlassian.com/display/DOCS/Introduction+to+the+Atlassian+Plugin+SDK
---
+++ src/main/java/com/rehabstudio/plugins/AddRehabProjectHook.java
@@ -0,0 +1,30 @@
+package com.rehabstudio.plugins;
+ 
+import com.atlassian.jira.blueprint.api.AddProjectHook;
+import com.atlassian.jira.blueprint.api.ConfigureData;
+import com.atlassian.jira.blueprint.api.ConfigureResponse;
+import com.atlassian.jira.blueprint.api.ValidateData;
+import com.atlassian.jira.blueprint.api.ValidateResponse;
+ 
+public class AddRehabProjectHook implements AddProjectHook
+{
+    @Override
+    public ValidateResponse validate(final ValidateData validateData)
+    {
+        ValidateResponse validateResponse = ValidateResponse.create();
+        if (validateData.projectKey().equals("TEST"))
+        {
+            validateResponse.addErrorMessage("Invalid Project Key");
+        }
+ 
+        return validateResponse;
+    }
+ 
+    @Override
+    public ConfigureResponse configure(final ConfigureData configureData)
+    {
+        ConfigureResponse configureResponse = ConfigureResponse.create().setRedirect("/issues/");
+ 
+        return configureResponse;
+    }
+}
